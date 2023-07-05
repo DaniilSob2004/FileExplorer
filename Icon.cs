490191125ea306bc95e6c6ce8e2ad0e75c019e45
Daniil <Soboliev.danik@gmail.com>
2023-07-05 16:17:29 +0300

    Craete Icon.cs

---
+++ Icon.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace FileExplorer
+{
+    public static class Icon
+    {
+        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr SHGetFileInfo(
+        string pszPath,
+        uint dwFileAttributes,
+        ref SHFILEINFO psfi,
+        uint cbFileInfo,
+        uint uFlags
+        );
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool DestroyIcon(IntPtr hIcon);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public int iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+        }
+
+        private const uint SHGFI_ICON = 0x000000100;
+        private const uint SHGFI_LARGEICON = 0x000000000;
+        private const uint SHGFI_SMALLICON = 0x000000001;
+
+
+        public static ImageSource GetFileIcon(string filePath)
+        {
+            IntPtr iconHandle = GetIconHandle(filePath, true);
+
+            ImageSource iconSource = Imaging.CreateBitmapSourceFromHIcon(
+                iconHandle,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions()
+            );
+
+            DestroyIcon(iconHandle);
+            return iconSource;
+        }
+
+        private static IntPtr GetIconHandle(string filePath, bool largeIcon)
+        {
+            SHFILEINFO shinfo = new SHFILEINFO();
+            uint flags = SHGFI_ICON;
+
+            if (largeIcon) flags |= SHGFI_LARGEICON;
+            else flags |= SHGFI_SMALLICON;
+            SHGetFileInfo(filePath, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), flags);
+
+            return shinfo.hIcon;
+        }
+    }
+}
